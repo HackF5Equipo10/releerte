123aa5118ae9cda16c8e63579d27b596ca12c784
Elvia Benedith <ejbenedith@gmail.com>
2023-02-23 23:05:22 +0100

    Link react router Dom

--- src/Components/Books/BooksComponents.jsx
+++ src/Components/Books/BooksComponents.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
-import {Typography, Modal,Box,Link, }from '@mui/material';
+import {Typography, Modal, Box }from '@mui/material';
 import Stack from '@mui/material/Stack';
 
 function BooksComponents({imagen, titulo, donación, autores, id}) {
