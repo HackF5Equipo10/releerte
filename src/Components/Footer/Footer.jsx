c40efc96f311e5fa15df5a1fa01bb5b1563696cd
Elvia Benedith <ejbenedith@gmail.com>
2023-02-24 02:56:09 +0100

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import caixalogo from '../../assets/img/caixa_bank_logo.png';
+import instagram from '../../assets/img/Vector.svg';
+import facebook from '../../assets/img/facebook.svg';
+import contact from '../../assets/img/send.svg';
+import './Footel.style.css'
 
-const Footer = () => {
-    return (
-    <>
-        <h3>FOOTER</h3>
-    </>
-    )
+export default function Footer() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+            <div className='icons-footer-container'>
+                <span><sub>@2023 Releerte</sub></span>
+                <img id='caixaIcon' src={caixalogo} alt='logo Caixa Bank'/>
+                <div className='social-media'>
+                    <img src={instagram} alt='instagram logo' />
+                    <img src={facebook} alt='instagram logo' />
+                    <img src={contact} alt='instagram logo' />
+                </div>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Footer
